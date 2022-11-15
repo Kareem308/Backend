cb42a6776431cca160452583fdbca61cf75b17c9
Kareem <Kareem@test.com>
2022-11-15 14:24:12 +0300

    improve output result

--- Calculator.js
+++ Calculator.js
@@ -2,15 +2,15 @@ var promptK = require('prompt');
 promptK.start();
 promptK.get(['num1', 'op', 'num2'], function (err, any) {
     if (any.op === '+') {
-        console.log(Number(any.num1) + Number(any.num2));
+        console.log((any.num1) + (any.op) + (any.num2), "=", Number(any.num1) + Number(any.num2));
     }
     if (any.op === '-') {
-        console.log(Number(any.num1) - Number(any.num2));
+        console.log((any.num1) + (any.op) + (any.num2), "=", Number(any.num1) - Number(any.num2));
     }
     if (any.op === '*') {
-        console.log(Number(any.num1) * Number(any.num2));
+        console.log((any.num1) + (any.op) + (any.num2), "=", Number(any.num1) * Number(any.num2));
     }
     if (any.op === '/') {
-        console.log(Number(any.num1) / Number(any.num2));
+        console.log((any.num1) + (any.op) + (any.num2), "=", Number(any.num1) / Number(any.num2));
     }
 });
--- Calculator.ts
+++ Calculator.ts
@@ -4,19 +4,18 @@ promptK.start();
 
 promptK.get(['num1','op', 'num2'], function (err, any) {
     if (any.op ==='+'){
-        console.log(Number(any.num1)+ Number(any.num2 ));
-
+        console.log((any.num1) + (any.op) + (any.num2 ) ,"=", Number(any.num1) + Number(any.num2 ));
     }
     if (any.op ==='-'){
-        console.log(Number(any.num1)- Number(any.num2 ));
+        console.log((any.num1) + (any.op) + (any.num2 ) ,"=", Number(any.num1)- Number(any.num2 ));
 
     }
     if (any.op ==='*'){
-        console.log(Number(any.num1)* Number(any.num2 ));
+        console.log((any.num1) + (any.op) + (any.num2 ) ,"=", Number(any.num1)* Number(any.num2 ));
 
     }
     if (any.op ==='/'){
-        console.log(Number(any.num1)/ Number(any.num2 ));
+        console.log((any.num1) + (any.op) + (any.num2 ) ,"=", Number(any.num1)/ Number(any.num2 ));
 
     }
 })
